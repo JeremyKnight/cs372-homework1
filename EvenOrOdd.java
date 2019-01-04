fd9b6b36f93c06de6a5ca997eadf125cbc2395f3
JeremyKnight <jeremyknightjck99@gmail.com>
2019-01-04 13:44:14 -0800

    first homework problem done

--- EvenOrOdd.java
+++ EvenOrOdd.java
@@ -1,7 +1,19 @@
-import java.util.scanner 
+import java.util.Scanner;
 
 public class EvenOrOdd {
 	public static void main(String args[]) {
-		
+		Scanner scnr = new Scanner(System.in);
+		int number=0;
+
+		System.out.println("enter a number between 10 and 100");
+		number = Integer.parseInt(scnr.nextLine());
+
+		for(int i=0; i<number; i++){
+			if(i%2==0)
+				System.out.println(i + " Even");
+			else
+				System.out.println(i + " Odd");
+		}
+
 	}
 }
