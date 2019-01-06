b423806f55f61b16814ec37baf3cdb857e84857c
Peter Tucker <ptucker@whitworth.edu>
2019-01-06 14:54:03 -0800

    feedback

--- EX1_4.java
+++ EX1_4.java
@@ -8,6 +8,9 @@ public class EX1_4 {
         double height=0;
 
         System.out.println("Enter your weight in pounds: ");
+        //PT -- name these numbers:
+        //   final double POUNDS_PER_KG: 0.453592.
+        // PT -- also, why nextLine rather than nextDouble? -1
         weight = Double.parseDouble(scnr.nextLine())*0.453592;
         System.out.println("Enter your height in inches: ");
         height = Double.parseDouble(scnr.nextLine())*0.0254;
